78f137046821a88ec30380b90e3e9034ff10b18c
mineng <s.minhaeng@gmail.com>
2019-03-10 12:06:21 +0900

    import05から

--- sample05.py
+++ sample05.py
@@ -3,9 +3,9 @@
 def add(a: int, b: int) -> int:
     return a + b
 
-y = add(a=3, b=4)
+#y = add(a=3, b=4)
 
-print(y) # ここで7が出力されればOK!!
+#print(y) # ここで7が出力されればOK!!
 
 
 # 課題2: 2つの整数の差を返す関数を定義してね
@@ -13,10 +13,20 @@ print(y) # ここで7が出力されればOK!!
 def diff(a: int, b: int) -> int:
     return a - b
 
-y1 = diff(a=4, b=3)
+#y1 = diff(a=4, b=3)
 
-print(y1)
+#print(y1)
 
 #subはサブストラクションの略
 #先にy1から考えてdef(関数)を作る ※関数の設計、テスト駆動開発
 #y1のdiffの真ん中でコントロール+Tを押したら変えれる(Renameする)
+
+
+
+if __name__ == '__main__':
+
+    y = add(a=3, b=4)
+    print(y)
+
+    y1 = diff(a=4, b=3)
+    print(y1)
---
+++ sample06.py
@@ -0,0 +1,8 @@
+import sample05
+
+if __name__ == '__main__':
+    y = sample05.add(a=15, b=7)
+    print(y)
+
+    y1 = sample05.diff(a=33, b=18)
+    print(y1)
