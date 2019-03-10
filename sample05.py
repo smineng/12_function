4678aaf43f1fdd1c734a5d857cae083381dd6a8a
mineng <s.minhaeng@gmail.com>
2019-03-10 11:48:45 +0900

    2つの整数の差を返す関数を定義

--- sample05.py
+++ sample05.py
@@ -6,3 +6,17 @@ def add(a: int, b: int) -> int:
 y = add(a=3, b=4)
 
 print(y) # ここで7が出力されればOK!!
+
+
+# 課題2: 2つの整数の差を返す関数を定義してね
+
+def diff(a: int, b: int) -> int:
+    return a - b
+
+y1 = diff(a=4, b=3)
+
+print(y1)
+
+#subはサブストラクションの略
+#先にy1から考えてdef(関数)を作る ※関数の設計、テスト駆動開発
+#y1のdiffの真ん中でコントロール+Tを押したら変えれる(Renameする)
