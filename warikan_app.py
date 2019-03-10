f48997f2f8d17101a86735f488f8dcbdc1020429
mineng <s.minhaeng@gmail.com>
2019-03-10 10:32:12 +0900

    割り勘アプリ2

---
+++ warikan_app.py
@@ -0,0 +1,8 @@
+amount = int(input("合計いくら？: "))
+number_of_people = int((input("何人？: ")))
+
+payment = amount // number_of_people
+remainder = amount % number_of_people
+
+print(f"1人: {payment}円, 端数:{remainder}円")
+
